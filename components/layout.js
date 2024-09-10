551469884b876c98074cd50f5c896274e40d9a57
Aldyne Ronquillo <aldyneronquillo91403@gmail.com>
2024-09-10 14:45:27 +0800

    first commit

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Aldyne A. Ronquillo';
+const name = 'Aldyne Ronquillo';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,27 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[I am Aldyne A. Ronquillo]</p>
+        
+        <p>10 Things That Require Zero Talent</p>
+
+        <ul>
+
+            <li>Being On Time</li>
+            <li>Making An Effort</li>
+            <li>Being High Energy</li>
+            <li>Having A Positive Attitude</li>
+            <li>Being Passionate</li>
+            <li>Using Good Body Language</li>
+            <li>Being Coachable</li>
+            <li>Doing A Little Extra</li>
+            <li>Being Prepared</li>
+            <li>Having A Strong Work Ethic</li>
+
+        </ul>
+
+
+
+
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
